2ffaf3707bc77212904089281abbf93d73b7a0f3
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-11 06:01:12 -0500

    update post-routes.js

--- post-routes.js
+++ post-routes.js
@@ -21,3 +21,46 @@ router.get('/', (req, res) => {
     res.status(500).json(err);
   });
 });
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'title', 'body', 'user_id'],
+    include: [
+      {
+          model: Comments,
+          as: 'comments',
+          attributes: ['id', 'comment_text', 'user_id'],
+        },
+    ],
+})
+  .then(dbPostData => {
+      if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id!' });
+          return;
+      }
+        res.json(dbPostData);
+    })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  Post.create({
+    title: req.body.title,
+    body: req.body.body,
+    user_id: req.session.user_id
+  })
+    .then(dbPostData => {
+        res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
