31811730657427fb7d004ece79a5b3b4ba24702a
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-27 00:38:51 -0500

    hard coded mysql information

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config();
 
-// create connection to our database, pass in your MySQL information 
+// creates connection to database, pass in MySQL information 
 //module.exports = sequelize;
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize('tech_blog', 'root', 'xTitan91!', {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
