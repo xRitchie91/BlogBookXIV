58b05f354f7d24b1732098d6b0b9b0e7d2cd75ff
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-27 00:51:06 -0500

    re-uploaded server.js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./controllers/api');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
