a21dca762ad8e3e0ce1e449b3994fa1208b63b90
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-28 20:23:47 -0500

    updated routes and path error in server

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Import all the models
-const User = require("./User");
-const Post = require("./Post");
-const Comment = require("./Comment");
+const User = require("./user");
+const Post = require("./post");
+const Comment = require("./comment");
 
 // create associations
 User.hasMany(Post, {
